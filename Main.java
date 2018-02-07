305224a881345582c9273f852a383edcabdad2d4
Bogdan <bhelpdig@gmail.com>
2018-02-07 19:01:29 +0300

    xcvsdf

--- Main.java
+++ Main.java
@@ -5,20 +5,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class Main {
     private static WebDriver driver;
-
-    public static void loginF(WebDriver driver, String login, String pass){
+    private static WebElement err;
+    public static void loginF(WebDriver driver, String login, String pass,int iIter){
         WebElement loginInput = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.name("Login")));
         WebElement passInput = (new WebDriverWait(driver, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.name("Password")));
         loginInput.sendKeys(login);
-        passInput.sendKeys(pass);
+        loginInput.sendKeys(pass);
         loginInput.submit();
     }
 
@@ -28,9 +26,16 @@ public class Main {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://m.mail.ru/login");
-        while(true){
-            loginF(driver,"origamycpa","123");
+
+        int iIter = 0;
+
+        while (true){
+            loginF(driver,"orygamimy","KmUoP12",iIter);
+            err = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.className("error")));
+            driver.get("https://m.mail.ru/login");
+            iIter++;
         }
+
 
     }
 }
